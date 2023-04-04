436196c2f5da36a1c5f83623dca1ad57a889c938
Vitolus <vito.davide00@gmail.com>
2023-04-05 01:23:26 +0200

    [SL-29] added DateTimeFilter: need to resolve TODOs

--- src/components/filter/DateTimeFilterUI.tsx
+++ src/components/filter/DateTimeFilterUI.tsx
@@ -1,28 +1,42 @@
-import { Card } from "primereact/card";
-import { ListBox } from "primereact/listbox";
-import { Button } from "primereact/button";
-import { observer } from "mobx-react-lite";
-import { DateTimeFilteringStrategy } from "../../filters/DateTimeFilter";
+import {Card} from "primereact/card";
+import {Button} from "primereact/button";
+import {observer} from "mobx-react-lite";
+import {Calendar} from "primereact/calendar";
+import {DateTimeFilteringStrategy} from "../../filters/DateTimeFilter";
 
 type ObserverProps = {
   filter: DateTimeFilteringStrategy;
 };
 
-const DateTimeFilterObserverUi = observer(({ filter }: ObserverProps) => (
-  <ListBox
-    options={filter.filterableDateTimes}
-    multiple
-    filter
-    value={filter.selectedDateTimes}
-    onChange={(e) => (filter.selectedDateTimes = e.value)}
-    listStyle={{ height: "300px" }}
+const DateTimeFilterObserverUi = observer(({filter}: ObserverProps) => (
+  <Calendar
+    //TODO: si aggiorna subito impedendo selezione multipla (range):
+    value={[new Date(filter.selectedDateTimes[0]),
+      new Date(filter.selectedDateTimes[filter.selectedDateTimes.length - 1])]}
+    onChange={(e) => {
+      const date = e.value as Date[];
+      filter.selectNone();
+      //TODO: 90% sbagliato, trovare modo migliore
+      date.forEach((e) => filter.selectedDateTimes.push(e.toISOString()));
+    }}
+    minDate={new Date(filter.filterableDateTimes[0])}
+    maxDate={new Date(filter.filterableDateTimes[filter.filterableDateTimes.length - 1])}
+    showIcon
+    selectionMode="range"
+    /*TODO: fix time and add these options
+    showTime
+    showSeconds
+    showMillisec
+    hourFormat="24"
+    */
+    //readOnlyInput
   />
 ));
 
 export const DateTimeFilterUi = (filter: DateTimeFilteringStrategy) => {
-  const DateTimeCardTitle = (
+  const dateTimeCardTitle = (
     <div className="flex align-items-center justify-content-between">
-      Filter by DateTime
+      Filter by Date and Time
       <div>
         <Button
           className="mx-4"
@@ -31,22 +45,16 @@ export const DateTimeFilterUi = (filter: DateTimeFilteringStrategy) => {
         />
         <Button
           className="mx-4"
-          label={filter.filterableDateTimes[0]}
+          label="Select None"
           onClick={filter.selectNone}
         />
-        <input
-          type="date"
-          className="mx-4"
-          value={filter.filterableDateTimes[0]}
-        />
-        <input type="date" className="mx-4" />
       </div>
     </div>
   );
   return (
-    <div className="p-1 col-4" key="firmware-filter">
-      <Card className="h-full" title={DateTimeCardTitle}>
-        <DateTimeFilterObserverUi filter={filter} />
+    <div className="p-1 col-4" key="datetime-filter">
+      <Card className="h-full" title={dateTimeCardTitle}>
+        <DateTimeFilterObserverUi filter={filter}/>
       </Card>
     </div>
   );
