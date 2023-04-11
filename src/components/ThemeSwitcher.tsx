10df76079f10f92ca4cb1f4abd7e42b9f6155163
Augusto Zanellato <augusto.zanellato@gmail.com>
2023-04-11 17:32:00 +0200

    Misc fixes (#13)
    * use PropsWithChildren instead of custom types
    
    * Square brackets for unit/subunit, svg chart renderer
    
    * fix build on windows
    
    Fuck windows, all my homies hate windows
    
    * make prettier happy again

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import * as echarts from "echarts";
 import { InputSwitch } from "primereact/inputswitch";
 import echartsDarkTheme from "../themes/dark.json";
@@ -48,11 +54,7 @@ function useTheme(): ThemeState {
   ];
 }
 
-type ThemeProviderProps = {
-  children: string | JSX.Element | JSX.Element[];
-};
-
-export function ThemeProvider({ children }: ThemeProviderProps) {
+export function ThemeProvider({ children }: PropsWithChildren) {
   const [theme, setTheme] = useTheme();
   return (
     <ThemeContext.Provider value={[theme, setTheme]}>
