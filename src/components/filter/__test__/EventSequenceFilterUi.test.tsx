ed1176ed0643f5cc978588bf7639bd310556a39d
MrBrune01 <brunello.mattia2001@gmail.com>
2023-04-26 00:09:50 +0200

    Ho provato ma ho fallito

--- src/components/filter/__test__/EventSequenceFilterUi.test.tsx
+++ src/components/filter/__test__/EventSequenceFilterUi.test.tsx
@@ -1,27 +1,33 @@
-import { describe, expect, test, jest } from '@jest/globals';
+import { describe, expect, test, jest } from "@jest/globals";
 import { render, screen } from "@testing-library/react";
-import { EventSequenceFilterUi } from '../EventSequenceFilterUi';
+import { EventSequenceFilterUi } from "../EventSequenceFilterUi";
 
-import { EventSequenceFilteringStrategy } from '../../../filters/EventSequenceFilter';
-jest.mock('../../../filters/EventSequenceFilter');
+import { EventSequenceFilteringStrategy } from "../../../filters/EventSequenceFilter";
+jest.mock("../../../filters/EventSequenceFilter");
+import logs from "../../../filters/__test__/logs.json";
+import {
+  LogParserResponse_4dfe1dd_LogEntry,
+  LogParserResponse_4dfe1dd_LogFile,
+} from "../../../openapi";
+type LogFile = LogParserResponse_4dfe1dd_LogFile;
+const logFile: LogFile = logs;
 
-const logFile = {
-    filename: "filename",
-    pc_datetime: "",
-    ups_datetime: "",
-    units_subunits: {},
-    log_entries: [],
-};
+describe("EventSequenceFilterUi", () => {
+  let filter: EventSequenceFilteringStrategy;
 
-it("Select add EventSequenceFilterUi", () => {
-    /*const filter = new EventSequenceFilteringStrategy(logFile);
+  beforeEach(() => {
+    filter = new EventSequenceFilteringStrategy(logFile);
     render(EventSequenceFilterUi(filter));
+  });
 
-    const btnElement = screen.getByText(/add/i);
-    btnElement.click();
-    expect(filter.addItem).toBeCalled();*/
-
-}); 
-
-
-
+  it("Add calls addItem", () => {
+    const buttons = screen.getAllByText(/Add/i);
+    for (const b of buttons) {
+      b.click();
+    }
+    expect(filter.addItem).toBeCalledTimes(buttons.length);
+  });
+  it("Table exist EventSequenceFilterUi", () => {
+    const table = screen.getByRole("table");
+  });
+});
