b1e1baededad5e43bf54230fe67bf3a6acd432b1
auli <andrea.auletta2001@gmail.com>
2023-04-27 22:47:22 +0200

    test logTable

---
+++ src/components/viewer/__test__/LogTable.test.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from "@testing-library/react";
+import { LogTable } from "../LogTable";
+import { LogParserResponse_4dfe1dd_LogFile } from "../../../openapi";
+import { LogParserResponse_4dfe1dd_LogEntry } from "../../../openapi";
+import LogFilteringService, { ILogFilteringService, LogFilteringStrategy } from "../../../services/LogFilteringService";
+import logs from "../../../filters/__test__/logs.json";
+import { entries } from "mobx";
+
+const LFS = (entries:LogParserResponse_4dfe1dd_LogEntry[]) => ({
+    register: function (filter: LogFilteringStrategy, ui: JSX.Element): void {
+        throw new Error("Function not implemented.");
+    },
+    filteredEntries: entries,
+    resetAll: function (): void {
+        throw new Error("Function not implemented.");
+    },
+    setLogFile: function (logFile: LogParserResponse_4dfe1dd_LogFile): void {
+        throw new Error("Function not implemented.");
+    },
+    removeFilters: function (): void {
+        throw new Error("Function not implemented.");
+    },
+    filtersUi: []
+} as ILogFilteringService);
+
+it("LogTable", () => {
+
+    
+    const lfs = LFS(logs.log_entries);
+    render(<LogFilteringService logFilteringService={lfs}><LogTable/></LogFilteringService>);
+
+
+    
+    logs.log_entries.forEach(entry => {
+        ["timestamp",
+        "unit",
+        "subunit",
+        "ini_filename",
+        "code",
+        "description",
+        "value"].forEach(field=>{
+            const element = screen.getAllByText(new RegExp((entry as any)[field], "i"));
+            expect(element[0]).toBeInTheDocument();
+
+        })
+        
+    });
+
+
+});
--- src/services/LogFilteringService.tsx
+++ src/services/LogFilteringService.tsx
@@ -76,10 +76,14 @@ const LogFilteringServiceContext = createContext<
   ILogFilteringService | undefined
 >(undefined);
 
-const LogFilteringService = (props: PropsWithChildren) => {
+type LogFilteringServiceProps = {
+  logFilteringService?: ILogFilteringService
+} & PropsWithChildren
+
+const LogFilteringService = (props: LogFilteringServiceProps) => {
   return (
     <LogFilteringServiceContext.Provider
-      value={new ConcreteLogFilteringService()}
+      value={props.logFilteringService ?? new ConcreteLogFilteringService()}
     >
       {props.children}
     </LogFilteringServiceContext.Provider>
