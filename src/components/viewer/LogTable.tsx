28452490da958af8cef81013b1cf211ba4afbfff
Vitolus <vito.davide00@gmail.com>
2023-05-02 19:49:29 +0200

    added reshade function for further polishing

--- src/components/viewer/LogTable.tsx
+++ src/components/viewer/LogTable.tsx
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
-import { stringToColor } from "../../utils";
+import { reshade, stringToColor } from "../../utils";
 import {
   ILogFilteringService,
   useLogFilteringService,
