09363dca82b2f989cf841095d8262e7d54522575
Augusto Zanellato <augusto.zanellato@gmail.com>
2023-05-13 16:16:15 +0200

    firmware mvvm fix

--- src/viewmodels/filters/FirmwareFilterViewModel.tsx
+++ src/viewmodels/filters/FirmwareFilterViewModel.tsx
@@ -4,8 +4,8 @@ import { FirmwareFilteringStrategy } from "../../filters/FirmwareFilter";
 export type IFirmwareFilterViewModel = ReturnType<typeof FirmwareFilterViewModel>;
 
 export const FirmwareFilterViewModel = (filter: FirmwareFilteringStrategy) => ({
-    selectAll: () => filter.selectAll,
-    selectNone: () => filter.selectNone,
+    selectAll: filter.selectAll,
+    selectNone: filter.selectNone,
     options: filter.filterableFirmwares,
     selection: () => filter.selectedFirmwares,
     onSelectionChange: (e: ListBoxChangeEvent) => filter.setSelection(e.value)
