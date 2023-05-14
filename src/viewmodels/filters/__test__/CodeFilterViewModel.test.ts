21b0fde589f70fc2f520dfb6fdbdc591d83058e4
Augusto Zanellato <augusto.zanellato@gmail.com>
2023-05-14 18:09:23 +0200

    code filter viewmodel tests

---
+++ src/viewmodels/filters/__test__/CodeFilterViewModel.test.ts
@@ -0,0 +1,44 @@
+import { CodeFilterViewModel, ICodeFilterViewModel } from "../CodeFilterViewModel"
+import { CodeFilteringStrategy } from "../../../filters/CodeFilter";
+import { ListBoxChangeEvent } from "primereact/listbox";
+
+jest.mock("../../../filters/CodeFilter");
+
+describe("CodeFilterViewModel", () => {
+    let viewModel: ICodeFilterViewModel;
+    let filter: jest.Mocked<CodeFilteringStrategy>;
+
+    beforeEach(() => {
+        filter = new CodeFilteringStrategy({} as any) as typeof filter;
+        viewModel = CodeFilterViewModel(filter);
+    });
+
+    it("filterableCodes", () => {
+        const filterableCodes = ["code1", "code2"];
+        Object.defineProperty(filter, "filterableCodes", { value: filterableCodes });
+        expect(viewModel.filterableCodes()).toBe(filterableCodes);
+    })
+
+    it("selectedCodes", () => {
+        filter.selectedCodes = ["code1"];
+        expect(viewModel.selectedCodes()).toBe(filter.selectedCodes);
+    })
+
+    it("onSelectAllClick", () => {
+        viewModel.onSelectAllClick();
+        expect(filter.selectAll).toBeCalled();
+    })
+
+    it("onSelectNoneClick", () => {
+        viewModel.onSelectNoneClick();
+        expect(filter.selectNone).toBeCalled();
+    })
+
+    it("onSelectionChange", () => {
+        const selection = ["code1"];
+        const event = {value: selection} as ListBoxChangeEvent;
+        viewModel.onSelectionChange(event);
+        expect(filter.setSelection).toBeCalledWith(selection)
+    })
+
+})
